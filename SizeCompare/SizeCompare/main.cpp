a4eaa1b6492946a8d07adec6045b89e8bc879759
Von VanMany <vonvanmany@gmail.com>
2023-04-24 18:16:43 -0400

    inital Build

---
+++ SizeCompare/SizeCompare/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SizeCompare.cpp
+ *
+ * Created: 4/24/2023 6:15:53 PM
+ * Author : berry
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
